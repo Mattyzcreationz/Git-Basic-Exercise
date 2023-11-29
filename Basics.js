64e0806d161b9b2e668ee94d2a2d7b5b27d7b914
Mattyzcreationz <141065370+Mattyzcreationz@users.noreply.github.com>
2023-11-28 16:39:22 -0800

    Add files via upload

---
+++ Basics.js
@@ -0,0 +1,53 @@
+//Create a folder called learn_git_again
+//Asnwer
+mkdir learn_git_again
+
+//cd into the learn_git_again folder
+
+//Answer
+cd learn_git_again
+
+//Create a file called third.txt
+
+//Answer
+touch third.txt
+
+//Initialize an empty git repositroy
+
+//Answer
+git init Empty
+
+//Add third.txt to the staging area.
+
+//Answer
+git third.txt add
+
+//Commit with this message "adding fourth.txt"
+
+//Answer
+git commit add - m "adding fourth.txt"
+
+//Remove the third.txt file
+
+//Answer
+rm third.txt
+
+//Add this change to the staging area
+
+//Answer
+git log
+
+//Change your global setting to core.paper=cat - you can read more about that in ink
+
+//Answer
+
+//Write the command to list all of the global
+//configurations for git on your machine.
+//You can type git config --global to find oout how to do this
+
+//Answer
+git config--global--list
+
+
+
+
